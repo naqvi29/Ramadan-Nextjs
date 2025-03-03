5411d31f40c2f6c4442595e154fa300f366df763
Zamanat <mali29april@gmail.>
2025-03-03 14:14:38 +0500

    gfs leaderboard update

--- components/gfs/Leaderboard.js
+++ components/gfs/Leaderboard.js
@@ -30,91 +30,97 @@ export default function LeaderboardGFS({
   }
 
   return (
-    <div className="bgRamadanLBFGS d-flex flex-column text-white">
-  <div className="container">
-    <div className="row px-5">
-      <div className="col-4 px-2">
-        <div className="row d-flex flex-column align-items-center justify-content-center">
-          <div className="text-center">
-            <img
-              src="/bg/gfs/logoGFS.png"
-              className="img-fluid logo-leaderboard"
-              width="38%"
-              alt="Logo"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="col-8">
-        <div className="text-center font-shadow mt-2">
-          <h5 className="city-font-longBanner">{city} <span> ({date})</span></h5>
-          {/* <p className="small date-font-longBanner"></p> */}
-        </div>
-        {/* Wrap the two tables in a new row */}
-        <div className="row mt-2">
-          <div className="col-6 d-flex justify-content-end align-items-center col-two-longBanner">
-            {/* Iftari Table */}
-            <div className="sm-font">
-              <table
-                className="table text-center mb-0"
-                style={{ borderSpacing: "2px", borderCollapse: "separate" }}
-              >
-                <tbody>
-                  <tr className="text-center">
-                    <td
-                      className="text-left md-font-longBanner align-middle border-0 head-color"
-                      rowSpan="2"
-                    >
-                      Iftari
-                    </td>
-                    <td className="span-bg-longBanner font-heading">HANAFI</td>
-                    <td className="span-bg-longBanner time">{iftariTime}</td>
-                  </tr>
-                  <tr className="text-center">
-                    <td className="span-bg-longBanner font-heading">JAFRI</td>
-                    <td className="span-bg-longBanner time">{iftariTimeJafria}</td>
-                  </tr>
-                </tbody>
-              </table>
+    <div className="bgRamadanLBFGS d-flex flex-column  text-white">
+    <div className="container">
+      <div className="row px-5">
+        <div className="col-4 px-2">
+    
+    
+          <div className="row d-flex flex-column align-items-center justify-content-center">
+            <div className="text-center">
+              <img
+                src="/bg/gfs/logoGFS.png"
+                className="img-fluid logo-gfs-leaderboard"
+                alt="Logo"
+              />
+            </div>
+    
+            <div className="text-center font-shadow mt-2">
+              <h5 className=" city-font-longBanner">{city}</h5>
+              <p className=" small date-font-longBanner">{date}</p>
             </div>
           </div>
-          <div className="col-6 d-flex justify-content-start align-items-center col-three-longBanner">
-            {/* Sehri Table */}
-            <div className="sm-font">
-              <table
-                className="table text-left mb-0"
-                style={{ borderSpacing: "2px", borderCollapse: "separate" }}
-              >
-                <tbody>
-                  <tr className="text-center">
-                    <td
-                      className="text-left md-font-longBanner align-middle border-0 head-color"
-                      rowSpan="2"
-                    >
-                      Sehri
-                    </td>
-                    <td className="span-bg-longBanner font-heading">HANAFI</td>
-                    <td className="span-bg-longBanner time">{sehriTime}</td>
-                  </tr>
-                  <tr className="text-center">
-                    <td className="span-bg-longBanner font-heading">JAFRI</td>
-                    <td className="span-bg-longBanner time">{sehriTimeJafria}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+    
+    
+        </div>
+    
+        <div className="col-4 d-flex justify-content-end align-items-center col-two-longBanner">
+          {/* Iftari Table */}
+          <div className="sm-font">
+            <table
+              className="table text-center mb-0"
+              style={{ borderSpacing: "2px", borderCollapse: "separate" }}
+            >
+              <tbody>
+                <tr className="text-center">
+                  <td
+                    className="text-left md-font-longBanner align-middle border-0 head-color"
+                    rowSpan="2"
+                  >
+                    Iftari
+                  </td>
+                  <td className="span-bg-longBanner font-heading">HANAFI</td>
+                  <td className="span-bg-longBanner time">{iftariTime} </td>
+                </tr>
+                <tr className="text-center">
+                  <td className="span-bg-longBanner font-heading">JAFRI</td>
+                  <td className="span-bg-longBanner time">
+                    {iftariTimeJafria}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
-        {/* Uncomment the note below if needed */}
-        {/*
-        <p className="p-note m-0 p-0 text-center">
+        <div className="col-4 d-flex justify-content-start align-items-center col-three-longBanner">
+          {/* Iftari Table */}
+          <div className="sm-font">
+            <table
+              className="table text-left mb-0"
+              style={{ borderSpacing: "2px", borderCollapse: "separate" }}
+            >
+              <tbody>
+                {/* Sehri Table */}
+    
+                <tr className="text-center">
+                  <td
+                    rowSpan="2"
+                    className="text-left md-font-longBanner align-middle border-0 head-color"
+                  >
+                    Sehri
+                  </td>
+                  <td className="span-bg-longBanner font-heading">HANAFI</td>
+                  <td className="span-bg-longBanner time">{sehriTime}</td>
+                </tr>
+                <tr className="text-center">
+                  <td className="span-bg-longBanner font-heading">JAFRI</td>
+                  <td className="span-bg-longBanner time">
+                    {sehriTimeJafria}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        {/* <p className="p-note m-0 p-0 text-center">
           Sehri and Iftar times may vary slightly (±1 min).
-        </p>
-        */}
+        </p> */}
       </div>
     </div>
-  </div>
-</div>
+    </div>
 
   );
 }
+
+
+
